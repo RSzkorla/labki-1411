2b29fbcc1ff3bee197b2f75687b3bc5e68f7ef8d
Radek <r.szkorla@gmail.com>
2016-11-14 12:19:35 +0100

    randomy

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@ namespace labki1411
 {
   static class ExtentionMethods
   {
+    static Random rng = new Random();
     public static double GetDouble(this string v) => Convert.ToDouble(v);
     public static int GetInt(this string v) => Convert.ToInt32(v);
     public static DateTime GetTime(this string v) => Convert.ToDateTime(v,new CultureInfo("pl-PL"));
+
+    public static double NextNewDouble(this Random v, double max) => v.NextDouble() * max;
+
+    public static double NextNewDouble(this Random v, double min, double max) => v.NextDouble() * (max- min)+min;
 
 
   }
@@ -27,9 +32,19 @@ namespace labki1411
       //Console.WriteLine("podaj a");
       //int b = Console.ReadLine().GetInt();
       //Console.WriteLine($"b= {b}");
-      Console.WriteLine("podaj date");
-      var c = Console.ReadLine().GetTime();
-      Console.WriteLine(c);
+      //Console.WriteLine("podaj date");
+      //var c = Console.ReadLine().GetTime();
+      //Console.WriteLine(c);
+
+
+
+
+
+
+
+
+
+
       // kolos 28.11
       //podstawy
       //wyjątki
