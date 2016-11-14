495999ed1aa25c96f5fbd7df045288dbc7c0df14
Radek <r.szkorla@gmail.com>
2016-11-14 11:38:42 +0100

    kolos

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labki1411
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // kolos 28.11
+      //podstawy
+      //wyjątki
+      //parametry
+      //struktury
+      //interfejsy
+      //klasy
+      //tablice
+    }
+  }
+}
