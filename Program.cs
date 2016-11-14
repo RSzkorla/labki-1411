bff91d5d249dabc86f9b3f69953d88eb7762f1e6
Radek <r.szkorla@gmail.com>
2016-11-14 12:22:23 +0100

    2

--- Program.cs
+++ Program.cs
@@ -7,18 +7,19 @@ using System.Globalization;
 
 namespace labki1411
 {
-  static class ExtentionMethods
+  static class StringExtention
   {
     static Random rng = new Random();
     public static double GetDouble(this string v) => Convert.ToDouble(v);
     public static int GetInt(this string v) => Convert.ToInt32(v);
     public static DateTime GetTime(this string v) => Convert.ToDateTime(v,new CultureInfo("pl-PL"));
 
+  }
+  static class RandomExtensions
+  {
     public static double NextNewDouble(this Random v, double max) => v.NextDouble() * max;
 
-    public static double NextNewDouble(this Random v, double min, double max) => v.NextDouble() * (max- min)+min;
-
-
+    public static double NextNewDouble(this Random v, double min, double max) => v.NextDouble() * (max - min) + min;
   }
   class Program
   {
